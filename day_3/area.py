9e648a0338144bf3cfff3e9bba6b57c0ea4df87c
Freddy Rosa <321212545+freddyrosa16@users.noreply.github.com>
2026-08-28 08:29:11 -0400

    adding f string in the prints

--- day_3/area.py
+++ day_3/area.py
@@ -5,28 +5,28 @@ import math
 base = int(input("Enter the base of the triangle: "))
 height = int(input("Enter the height of the triangle: "))
 area = 0.5 * base * height
-print(area)
+print(f'The area of the triangle is {area}')
 
     # Perimeter of a triangle
 side_a = int(input("Enter side a of the triangle: "))
 side_b = int(input("Enter side b of the triangle: "))
 side_c = int(input("Enter side c of the triangle: "))
 perimeter = side_a + side_b + side_c
-print(perimeter)
+print(f'The perimeter of the triangle is {perimeter}')
 
     # Area and Perimeter of a Rectangle
 length = int(input("Enter the length of the Rectangle: "))
 height = int(input("Enter the height of the Rectangle: "))
 area = length * height
 perimeter = 2 * (length + height)
-print(area, perimeter)
+print(f'The area of the rectangle is {area} and the perimeter is {perimeter}')
 
     # Area and Circumference of a Circle
 pi = 3.14
 radius = int(input("Enter the radius of the circle: "))
 area = pi * radius * radius
 circumference = 2 * pi * radius
-print(area, circumference)
+print(f'The area of the circle is {area} and the circumference is {circumference}')
 
     # Find the slope, x-intercept and y-intercept of y = 2x -2
 slope_1 = 2
@@ -37,12 +37,12 @@ y_intercept = (slope_1 * x) - 2
 
 # X Intercept
 x_intercept = (0 - y_intercept) / slope_1
-print(slope_1, x_intercept, y_intercept)
+print(f'The slope is {slope_1}, the x-intercept is {x_intercept}, and the y-intercept is {y_intercept}')
 
     # Find slope m = y2-y1/x2-x1 and Euclidean distance between point (2, 2) and point (6,10)
 slope_2 = (10 - 2) / (6 - 2)
 euclidean = math.dist((2, 2), (6, 10))
-print(slope_2, euclidean)
+print(f'The slope is {slope_2} and the Euclidean distance is {euclidean}')
 
     # Comparing slopes
 print(slope_1 == slope_2)
@@ -50,7 +50,7 @@ print(slope_1 == slope_2)
     # Calculate the value of y (y = x^2 + 6x + 9) using different x values that make y = 0
 x = -3
 y = ((-3) ** 2) + (6 * -3) + 9
-print(y)
+print(f'When x is {x}, y is {y}')
 
     # Find the length of 'python' and 'dragon' and make a falsy comparison statement.
 python = len('python')
@@ -68,7 +68,7 @@ print('on' not in 'python' and 'on' not in 'dragon')
 
     # Find the length of the text python and convert the value to float and convert it to string
 python = str(float(len('python')))
-print(python)
+print(f'The converted length of python is {python}')
 
     # I check if a number is even using n % 2 == 0.
 
@@ -107,10 +107,10 @@ hours = int(input('Enter the hours worked per week: '))
 rate = int(input('Enter the rate per hour: '))
 weekly = hours * rate
 pay = hours * rate * 52 / 12
-print(f'Your weekly rate is of {weekly}')
-print(f'Your monthly rate is of {pay}')
+print(f'Your weekly pay is {weekly}')
+print(f'Your average monthly pay is {pay}')
 
     # Prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
 years = int(input('Enter the number of years you have lived: '))
-seconds = 3600 * years * 365 * 24 * 60 * 60
+seconds = years * 365 * 24 * 60 * 60
 print(f'You have lived for {seconds} seconds')
