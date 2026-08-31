4456e5bd527057c3a39e060d02f0539cb30ea418
Freddy Rosa <321212545+freddyrosa16@users.noreply.github.com>
2026-08-31 11:26:09 -0400

    Practice list and dictionary conditionals

--- day_9/conditionals.py
+++ day_9/conditionals.py
@@ -54,13 +54,31 @@ elif score >= 0 and score <= 59:
         # December, January, February: Winter
         # March, April, May: Spring
         # June, July, August: Summer
-
+autumn = ['September', 'October', 'November']
+winter = ['December', 'January', 'February']
+spring = ['March', 'April', 'May']
+summer = ['June', 'July', 'August']
+month = input('Enter the month we are currently in: ')
+if month in autumn:
+    print('We are in Autumn')
+elif month in winter:
+    print('We are in Winter')
+elif month in spring:
+    print('We are in Spring')
+elif month in summer:
+    print('We are in Summer')
 
     # Use this fruit list and check a chosen fruit:
         # fruits = ['banana', 'orange', 'mango', 'lemon']
         # If the fruit is not in the list, add it and display the modified list.
         # If it is already present, display that the fruit already exists in the list.
-
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruit = input('Enter your favorite fruit: ')
+if fruit not in fruits:
+    fruits.append(fruit)
+    print(fruits)
+else:
+    print(f' We already have {fruit} in our {fruits} list')
 
     # Use this person dictionary for the following exercises. You may modify it:
         # person = {
@@ -75,20 +93,31 @@ elif score >= 0 and score <= 59:
         #         'zipcode': '02210'
         #     }
         # }
-
+person = {'first_name': 'Asabeneh', 'last_name': 'Yetayeh', 'age': 250, 'country': 'Finland', 'is_married': True, 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'address': { 'street': 'Space street', 'zipcode': '02210'}}
 
     # Check whether person contains the skills key. If it does, display the middle skill from the skills list.
-
+if 'skills' in person:
+   print(person['skills'][2])
 
     # Check whether person contains the skills key. If it does, check whether Python is one of the person's skills and display the result.
-
+if 'skills' in person:
+    print(True if 'Python' in person['skills'] else False)
 
     # Determine the person's developer title from their skills:
         # If the skills are only JavaScript and React, display "He is a front end developer".
         # If the skills include Node, Python, and MongoDB, display "He is a backend developer".
         # If the skills include React, Node, and MongoDB, display "He is a fullstack developer".
         # Otherwise, display "unknown title".
-
+if 'JavaScript' in person['skills'] and 'React' in person['skills'] and len(person['skills']) == 2:
+    print('He is a front end developer')
+elif 'Node' in person['skills'] and 'Python' in person['skills'] and 'MongoDB' in person['skills']:
+    print('He is a backend developer')
+elif 'React' in person['skills'] and 'Node' in person['skills'] and 'MongoDB' in person['skills']:
+    print('He is a fullstack developer')
+else:
+    print('unknown title')
 
     # If the person is married and lives in Finland, display this information using their name and country:
         # Asabeneh Yetayeh lives in Finland. He is married.
+if person['is_married'] == True and person['country'] == 'Finland':
+    print(f'{person['first_name']} {person['last_name']} lives in {person['country']}. He is married.')
